f4b93c90091f364221941d2095ea983738b648eb
robsv <svirskasr@janelia.hhmi.org>
2019-02-11 15:42:49 -0500

    Added daily_neuprint_stats.py

---
+++ bin/daily_neuprint_stats.py
@@ -0,0 +1,116 @@
+import argparse
+import datetime
+import json
+import sys
+import colorlog
+import requests
+
+
+# Configuration
+CONFIG = {'config': {'url': 'http://config.int.janelia.org/'}}
+
+
+def call_responder(server, endpoint, payload = ''):
+    """ Call a responder
+        Keyword arguments:
+        server: server
+        endpoint: REST endpoint
+        psyload: POST payload
+    """
+    url = CONFIG[server]['url'] + endpoint
+    try:
+        if payload:
+            headers = {"Content-type": "application/json", "Authorization": "Bearer " + CONFIG[server]['bearer']}
+            req = requests.post(url, headers=headers, json=payload)
+        else:
+            req = requests.get(url)
+    except requests.exceptions.RequestException as err:
+        logger.critical(err)
+        sys.exit(-1)
+    if req.status_code == 200:
+        return req.json()
+    else:
+        logger.error('Status: %s', str(req.status_code))
+        sys.exit(-1)
+
+
+def initialize_program():
+    """ Initialize databases """
+    global CONFIG
+    dbc = call_responder('config', 'config/rest_services')
+    CONFIG = dbc['config']
+
+
+def process_data(dataset):
+    datestruct = dict()
+    datasetn = dataset + '-Neuron'
+    suffix = ' RETURN count(n)'
+    # 0.5 Assign
+    payload = {"cypher": "MATCH (n:`" + datasetn + "`{status:\"0.5assign\"})" + suffix}
+    response = call_responder('neuprint', 'custom/custom', payload)
+    datestruct['0.5assign'] = response['data'][0][0]
+    logger.info('0.5assign: ' + str(datestruct['0.5assign']))
+    # Anchors
+    payload['cypher'] = "MATCH (n:`" + datasetn + "`{status:\"Anchor\"})" + suffix
+    response = call_responder('neuprint', 'custom/custom', payload)
+    datestruct['anchor'] = response['data'][0][0]
+    logger.info('anchor: ' + str(datestruct['anchor']))
+    # Traced neurons
+    payload['cypher'] = "MATCH (n:`" + datasetn + "`) WHERE n.status=\"Roughly traced\" OR n.status=\"Prelim Roughly traced\" OR n.status=\"Traced\"" + suffix
+    response = call_responder('neuprint', 'custom/custom', payload)
+    datestruct['traced'] = response['data'][0][0]
+    logger.info('traced: ' + str(datestruct['traced']))
+    # Top ROIs
+    payload['cypher'] = "MATCH (n:Meta:" + dataset + ") RETURN n.superLevelRois"
+    response = call_responder('neuprint', 'custom/custom', payload)
+    rois = response['data'][0][0]
+    roidict = dict()
+    count = 1
+    for roi in rois:
+        logger.info('Fetching data for ROI ' + roi + '(' + str(count) + '/' + str(len(rois)) + ')')
+	    # 0.5 Assign
+        payload['cypher'] = "MATCH (n:`" + datasetn + "`{`" + roi + "`:true}) WHERE n.status=\"0.5assign\"" + suffix
+        response = call_responder('neuprint', 'custom/custom', payload)
+        roiass = response['data'][0][0]
+        # Anchor
+        payload['cypher'] = "MATCH (n:`" + datasetn + "`{`" + roi + "`:true}) WHERE n.status=\"Anchor\"" + suffix
+        response = call_responder('neuprint', 'custom/custom', payload)
+        roianc = response['data'][0][0]
+        roidict[roi] = {'0.5assign': roiass, 'anchor': roianc}
+        count += 1
+    datestruct['rois'] = roidict
+    logger.info('Found data for ' + str(len(roidict)) + ' ROIs')
+    logger.debug(datestruct)
+    if ARG.WRITE:
+        today = datetime.datetime.today().strftime('%Y-%m-%d')
+        logger.warning("Writing " + dataset + " data to config system for " +
+                       today)
+        resp = requests.post(CONFIG['config']['url'] +
+        	                 'importjson/neuprint_' + dataset + '/' + today,
+                                 {"config": json.dumps(datestruct)})
+
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser(description="Write dataset daily stats to config system")
+    PARSER.add_argument('--dataset', dest='DATASET', action='store',
+                        default='hemibrain', help='Dataset')
+    PARSER.add_argument('--write', dest='WRITE', action='store_true',
+                        default=False, help='Write record to config system')
+    PARSER.add_argument('--verbose', dest='VERBOSE', action='store_true',
+                        default=False, help='Flag, Chatty')
+    PARSER.add_argument('--debug', dest='DEBUG', action='store_true',
+                        default=False, help='Flag, Very chatty')
+    ARG = PARSER.parse_args()
+
+    logger = colorlog.getLogger()
+    if ARG.DEBUG:
+        logger.setLevel(colorlog.colorlog.logging.DEBUG)
+    elif ARG.VERBOSE:
+        logger.setLevel(colorlog.colorlog.logging.INFO)
+    else:
+        logger.setLevel(colorlog.colorlog.logging.WARNING)
+    HANDLER = colorlog.StreamHandler()
+    HANDLER.setFormatter(colorlog.ColoredFormatter())
+    logger.addHandler(HANDLER)
+    initialize_program()
+    process_data(ARG.DATASET)
