5a7f60d7477e2dc48befc5444c6726bed0941775
robsv <svirskasr@janelia.hhmi.org>
2019-02-11 16:11:04 -0500

    pylint

--- bin/daily_neuprint_stats.py
+++ bin/daily_neuprint_stats.py
@@ -27,11 +27,11 @@ def call_responder(server, endpoint, payload = ''):
     except requests.exceptions.RequestException as err:
         logger.critical(err)
         sys.exit(-1)
-    if req.status_code == 200:
-        return req.json()
-    else:
+    if req.status_code != 200:
         logger.error('Status: %s', str(req.status_code))
         sys.exit(-1)
+    else:
+        return req.json()
 
 
 def initialize_program():
