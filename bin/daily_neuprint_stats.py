80176a5fd1dbffbde03a51bc843226365584a6b9
Rob Svirskas <svirskasr@janelia.hhmi.org>
2019-02-12 16:22:21 -0500

    Added new metrics to daily stats

--- bin/daily_neuprint_stats.py
+++ bin/daily_neuprint_stats.py
@@ -56,7 +56,7 @@ def process_data(dataset):
     datestruct['anchor'] = response['data'][0][0]
     logger.info('anchor: ' + str(datestruct['anchor']))
     # Traced neurons
-    payload['cypher'] = "MATCH (n:`" + datasetn + "`) WHERE n.status=\"Roughly traced\" OR n.status=\"Prelim Roughly traced\" OR n.status=\"Traced\"" + suffix
+    payload['cypher'] = "MATCH (n:`" + datasetn + "`) WHERE (n.status=\"Roughly traced\" OR n.status=\"Prelim Roughly traced\" OR n.status=\"Traced\")" + suffix
     response = call_responder('neuprint', 'custom/custom', payload)
     datestruct['traced'] = response['data'][0][0]
     logger.info('traced: ' + str(datestruct['traced']))
@@ -64,10 +64,15 @@ def process_data(dataset):
     payload['cypher'] = "MATCH (n:Meta:" + dataset + ") RETURN n.superLevelRois"
     response = call_responder('neuprint', 'custom/custom', payload)
     rois = response['data'][0][0]
+    # ROI info
+    payload['cypher'] = "MATCH (n:Meta:" + dataset + ") RETURN n.roiInfo"
+    response = call_responder('neuprint', 'custom/custom', payload)
+    totaldict = json.loads(response['data'][0][0])
+    #rois = totaldict.keys()
     roidict = dict()
     count = 1
     for roi in rois:
-        logger.info('Fetching data for ROI ' + roi + '(' + str(count) + '/' + str(len(rois)) + ')')
+        logger.info('Fetching data for ROI ' + roi + ' (' + str(count) + '/' + str(len(rois)) + ')')
 	    # 0.5 Assign
         payload['cypher'] = "MATCH (n:`" + datasetn + "`{`" + roi + "`:true}) WHERE n.status=\"0.5assign\"" + suffix
         response = call_responder('neuprint', 'custom/custom', payload)
@@ -76,7 +81,18 @@ def process_data(dataset):
         payload['cypher'] = "MATCH (n:`" + datasetn + "`{`" + roi + "`:true}) WHERE n.status=\"Anchor\"" + suffix
         response = call_responder('neuprint', 'custom/custom', payload)
         roianc = response['data'][0][0]
-        roidict[roi] = {'0.5assign': roiass, 'anchor': roianc}
+        # Complete
+        payload['cypher'] = "MATCH (n:`" + datasetn + "`{`" + roi + "`:true}) WHERE (n.status=\"Roughly traced\" OR n.status=\"Prelim Roughly traced\" OR n.status=\"Traced\" OR n.status=\"Leaves\")" + suffix
+        response = call_responder('neuprint', 'custom/custom', payload)
+        roicomp = response['data'][0][0]
+        roidict[roi] = {'0.5assign': roiass, 'anchor': roianc, "complete": roicomp}
+        # Traced neurons
+        payload['cypher'] = "MATCH (n:`" + datasetn + "`{`" + roi + "`:true}) WHERE (n.status = \"Roughly traced\" OR n.status = \"Prelim Roughly traced\" OR n.status = \"Leaves\" OR n.status = \"Traced\") WITH apoc.convert.fromJsonMap(n.roiInfo) AS roiInfo RETURN sum(roiInfo.`" + roi + "`.pre), sum(roiInfo.`" + roi + "`.post)"
+        response = call_responder('neuprint', 'custom/custom', payload)
+        roidict[roi].update({'presynaptic': response['data'][0][0], 'postsynaptic': response['data'][0][1]})
+        # Denomionators
+        if roi in totaldict:
+            roidict[roi].update({'presynaptic_total': totaldict[roi]['pre'], 'postsynaptic_total': totaldict[roi]['post']})
         count += 1
     datestruct['rois'] = roidict
     logger.info('Found data for ' + str(len(roidict)) + ' ROIs')
